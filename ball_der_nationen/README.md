f4f1c7248c1ddf94d68c79c448974e868e09c197
mn-gsc <84667612+mn-gsc@users.noreply.github.com>
2026-06-01 16:21:34 +0200

    initial commit
    contains fully tested and operational sitepackage for TYPO3 Version 14

---
+++ ball_der_nationen/README.md
@@ -0,0 +1,4 @@
+Site Package for the project "Ball der Nationen"
+==============================================================
+
+Add some explanation here.
---
+++ ball_der_nationen/ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+
+defined('TYPO3') or die('Access denied.');
+
+// Add default RTE configuration
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ball_der_nationen'] = 'EXT:ball_der_nationen/Configuration/RTE/Default.yaml';
